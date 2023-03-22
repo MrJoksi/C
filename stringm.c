b71ce0901dfdadc5861d3bbfaf13e5143488ecd6
Elmeri S <e2203113@edu.vamk.fi>
2023-03-22 21:01:09 +0200

    paivitys paivitys

--- stringm.c
+++ stringm.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <ctype.h>
+#define MAX_LENGTH_STRING 30
 
 void display_menu(void);
 char ask_command(void);
-int main(void){
-    
+char read_string(char read_str[]);
+char print_string(char print_str[MAX_LENGTH_STRING]);
+int main(void)
+{
+    char current_string[MAX_LENGTH_STRING] = "Hello world!";
     display_menu();
-    char string = ask_command();
+    print_string(current_string);
+    
+    while (character != 88)//Runs as long as "char string" isn't 'X(ascii 88)'
+    {
+        char character = ask_command();
+        if (character == 69)
+        {
+            printf("%s",current_string);
+        }
+        if (character == 70)
+        {
+            read_string()
+        }
+        
+        
+        
+        
+    }
+    
+    printf("%c",string);
 }
 
-void display_menu(void){
+void display_menu(void)
+{
     printf("A) Count the number of vowels in string\n");
     printf("B) Count the number of constants in the string\n");
     printf("C) Convert the string to uppercase\n");
@@ -18,10 +43,31 @@ void display_menu(void){
     printf("G) Read string from file\n");
     printf("H) Write string to file\n\n");
     printf("M) Display this menu\n");
-    printf("X) Exit this program");
+    printf("X) Exit this program\n");
 }
-char ask_command(void){
+
+char print_string(char print_str[MAX_LENGTH_STRING])
+{
+    printf("%s",print_str);
+}
+
+char ask_command(void)
+{
     char str[6];
-    fgets(str,6,stdin);
-    return str[0];
+    int i;
+    char ret_char;
+    while (i != 1)
+    {
+        printf("\n");
+        fgets(str,6,stdin);
+        ret_char = toupper(str[0]);
+        if (ret_char > 64 && ret_char < 91)//selvittää onko annettu arvo kirjain
+        {
+            i = 1;
+        }
+        
+    }
+    return ret_char;   
 }
+    
+    
